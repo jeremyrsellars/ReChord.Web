5c844d0f9c6e591dbf5c2b61587710e2fcc1254a
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-01-07 10:35:45 -0500

    default port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('coffee-script');
 var app = new (require('./webapp.coffee').WebApp)();
 app.configureWeb();
-require('http').createServer(app.app).listen(process.env.PORT);
+require('http').createServer(app.app).listen(process.env.PORT || 80);
