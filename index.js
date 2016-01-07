d7f7d8a6c10b6c39465ae34e839e0f2aa7f3e2d0
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-01-07 10:48:29 -0500

    run instead of listen... #azure

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('coffee-script');
 var app = new (require('./webapp.coffee').WebApp)();
-app.configureWeb();
-require('http').createServer(app.app).listen(process.env.PORT || 80);
+app.listen();
+// app.configureWeb();
+// require('http').createServer(app.app).listen(process.env.PORT || 80);
